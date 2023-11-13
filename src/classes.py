b67c1cd90daa991f15e5f3b16a1db5f3821137f9
Egor Nikitin <ega.nik5674@gmail.com>
2023-11-13 15:10:59 +0300

    Update classes.py

--- src/classes.py
+++ src/classes.py
@@ -1,26 +1,43 @@
 import json
 
 
+class User:
+    def __init__(self, name, email, password, passwordConfirmation):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+
+
 class UserRepository:
-    def __init__(self, file_path='data/users.json'):
+    def __init__(self, file_path='../data/users.json'):
         self.file_path = file_path
+        self.users = self.content()
 
     def content(self):
         try:
-            with open(self.file_path, 'r', encoding='utf-8') as file:
+            with open(self.file_path, "r", encoding="utf-8") as file:
                 return json.load(file)
-        except FileNotFoundError:
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
             return []
 
-    def save(self, user):
-        with open(self.file_path, 'w', encoding='utf-8') as file:
-            users = json.load(file)
-            update_users = users.append(user)
-            json.dump(update_users, file)
+    def save_users(self):
+        with open(self.file_path, 'w', encoding="utf-8") as file:
+            json.dump(self.users, file, ensure_ascii=False, indent=4)
 
-    def find(self, user_id):
-        with open(self.file_path, 'r', encoding='utf-8') as file:
-            users = json.load(file)
-            for user in users:
-                if user['id'] == user_id:
-                    return user
+    def save(self, user):
+        existing_usernames = [existing_user['Имя пользователя'] for existing_user in self.users]
+        if user.name in existing_usernames:
+            print(f"User with username '{user.name}' already exists.")
+        else:
+            self.users.append({
+                'Имя пользователя': user.name,
+                'Email': user.email,
+                'Пароль': user.password
+            })
+        self.save_users()
+
+    def find(self, name):
+        for user in self.users:
+            if user['Имя пользователя'] == name:
+                return user
