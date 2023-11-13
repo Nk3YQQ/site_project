17e608f1707f23f0b2aad56b8361dadf270845df
Egor Nikitin <ega.nik5674@gmail.com>
2023-11-13 15:11:54 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,21 @@
+def validate(user):
+    errors = {}
 
+    if not user.name:
+        errors['name'] = 'Не может быть пустым'
+
+    if not user.email:
+        errors['email'] = 'Не может быть пустым'
+    elif '@' not in user.email or '.' not in user.email:
+        errors['email'] = 'Некорректный формат email'
+
+    if not user.password:
+        errors['password'] = 'Не может быть пустым'
+
+    if len(user.password) <= 5:
+        errors['password'] = 'Слишком короткий пароль'
+
+    if user.password != user.passwordConfirmation:
+        errors['passwordConfirmation'] = 'Пароли не совпадают'
+
+    return errors
